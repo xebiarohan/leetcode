2a49a4e453eae901694b657a65c2869f34910285
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2025-07-20 13:24:02 +0400

    Tree problem

--- src/tree/BinaryTreeTilt.java
+++ src/tree/BinaryTreeTilt.java
@@ -7,17 +7,30 @@ public class BinaryTreeTilt {
 
     int finalResult = 0;
     public int findTilt(TreeNode root) {
-        if(root != null) {
-            finalResult = finalResult + Math.abs(sum(root.right) - sum(root.left));;
-            findTilt(root.left);
-            findTilt(root.right);
-        }
+        tilt(root);
         return finalResult;
     }
 
     public int sum(TreeNode node) {
         if(node != null) {
             return node.val + sum(node.left) + sum(node.right);
+        }
+        return 0;
+    }
+
+    public int tilt(TreeNode node) {
+        if(node != null) {
+            int tilt = Math.abs(tilt(node.left) - tilt(node.right));
+            finalResult = finalResult + tilt;
+            if(node.left != null && node.right != null) {
+                return node.val + node.left.val + node.right.val;
+            } else if(node.left == null && node.right == null) {
+                return node.val;
+            } else if(node.left == null) {
+                return node.val + node.right.val;
+            } else {
+                return node.val + node.left.val;
+            }
         }
         return 0;
     }
